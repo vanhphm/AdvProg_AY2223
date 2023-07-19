633bdef2692dd70d734735c0244cac179af4a6e5
vanhphm <138658451+vanhphm@users.noreply.github.com>
2023-07-19 10:50:22 +0700

    u

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -13,8 +13,9 @@ using namespace std;
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
-    return 100;
+    int randomNumber = rand()% 100 +1;
+    return randomNumber;
+   
 }
 
 
@@ -26,8 +27,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int guess;
+    cout <<"Enter your guess(1..100)";
+    cin >> guess;
+    return guess;
 }
 
 
@@ -45,8 +48,15 @@ string getAnswer(int number, int randomNumber) {
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
+    
     string answer;
-
+    if (number>randomNumber){
+        answer ="Your number is higher.";
+    }else if (number<randomNumber){
+        answer = "Your number is lower.";
+    }else{
+        answer = "Congratulation! You win.";
+    }
     return answer;
 }
 
@@ -59,8 +69,11 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer == "Congratulation! You win."){
+        return true;
+    }else {
+        return false;
+    }
 }
 
 
@@ -73,7 +86,8 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued == "y"||isContinued == "Y") {result=true; }
+    else if(isContinued == "n"||isContinued =="N"){result=false;}
     return result;
 }
 
@@ -87,7 +101,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cin >> isContinued;
     return isContinued;
 }
 
